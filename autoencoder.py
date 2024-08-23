a65fa1ebb71f098286a81dc088f15425fbc7160b
LeoUribC <leo.uribe1205@gmail.com>
2024-08-22 22:25:56 -0500

    finished main architecture and started mnist tests

--- autoencoder.py
+++ autoencoder.py
@@ -34,6 +34,7 @@ class Autoencoder:
         # private attributes
         self._num_conv_layers = len(conv_filters)
         self._shape_before_bottleneck = None
+        self._model_input = None
         
         self._build()
     
@@ -44,12 +45,33 @@ class Autoencoder:
         """
         self.encoder.summary()
         self.decoder.summary()
+        self.model.summary()
+    
+
+    def compile(self, learning_rate=0.0001):
+        optimizer = ks.optimizers.Adam(learning_rate=learning_rate)
+        mse_loss = ks.losses.MeanSquaredError()
+        self.model.compile(optimizer=optimizer, loss=mse_loss)
+    
+
+    def train(self, x_train, batch_size, num_epochs):
+        self.model.fit( x_train,
+                        x_train,
+                        batch_size=batch_size,
+                        epochs=num_epochs,
+                        shuffle=True )
 
     
     def _build(self):
         self._build_encoder()
         self._build_decoder()
-        #self._build_autoencoder()
+        self._build_autoencoder()
+    
+
+    def _build_autoencoder(self):
+        model_input = self._model_input
+        model_output = self.decoder( self.encoder(model_input) )
+        self.model = Model(model_input, model_output, name="autoencoder")
     
 
     def _build_decoder(self):
@@ -125,6 +147,7 @@ class Autoencoder:
         encoder_input = self._add_encoder_input()
         conv_layers = self._add_conv_layers(encoder_input)
         bottleneck = self._add_bottleneck(conv_layers)
+        self._model_input = encoder_input
         self.encoder = Model(encoder_input, bottleneck, name='encoder')
     
 
