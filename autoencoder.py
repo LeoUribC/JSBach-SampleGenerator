37967caf05bdb9ac7dcc6b8877ed1507bb4f5af1
LeoUribC <leo.uribe1205@gmail.com>
2024-09-01 20:56:48 -0500

    added reconstruct method

--- autoencoder.py
+++ autoencoder.py
@@ -75,6 +75,12 @@ class Autoencoder:
         self.model.load_weights(weights_path)
     
 
+    def reconstruct(self, images):
+        latent_representations = self.encoder.predict(images)
+        reconstructed_images = self.decoder.predict(latent_representations)
+        return reconstructed_images, latent_representations
+    
+
     @classmethod
     def load(cls, save_folder="."):
 
