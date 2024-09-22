4ab776a10a458f4803df20ffe5373e72851f43ed
LeoUribC <leo.uribe1205@gmail.com>
2024-09-22 12:28:56 -0500

    Implemented method to load spectrograms and modified params

--- train.py
+++ train.py
@@ -1,36 +1,42 @@
-from tensorflow.keras.datasets import mnist
+import os
+import numpy as np
 
 from autoencoder import VAE
 
 
 
 LEARNING_RATE = 0.0005
-BATCH_SIZE = 32
-EPOCHS = 100
+BATCH_SIZE = 64
+EPOCHS = 150
+
+SPECTROGRAMS_PATH = "/home/leo_uc/Documentos/2024_2/Enfasis_III/spectrograms/"
 
 
 
-def load_mnist():
+def load_fsdd(spectrograms_path):
     
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = []
     
-    x_train = x_train.astype("float32") / 255
-    x_train = x_train.reshape(x_train.shape + (1,))
-    x_test = x_test.astype("float32") / 255
-    x_test = x_test.reshape(x_test.shape + (1,))
-
-    return x_train, y_train, x_test, y_test
-
+    for root, _, file_names in os.walk(spectrograms_path):
+        for file_name in file_names:
+            file_path = os.path.join(root, file_name)
+            spectrogram = np.load(file_path)
+            x_train.append(spectrogram)
     
+    x_train = np.array(x_train)
+    x_train = x_train[..., np.newaxis]
+
+    return x_train
+
 
 def train(x_train, learning_rate, batch_size, epochs):
 
     autoencoder = VAE(
-        input_shape=(28, 28, 1),
-        conv_filters=(32, 64, 64, 64),
-        conv_kernels=(3, 3, 3 ,3),
-        conv_strides=(1, 2, 2, 1),
-        latent_space_dim=2
+        input_shape=(256, 64, 1),
+        conv_filters=(512, 256, 128, 64, 32),
+        conv_kernels=(3, 3, 3 ,3, 3),
+        conv_strides=(2, 2, 2, 2, (2, 1)),
+        latent_space_dim=128
     )
 
     autoencoder.summary()
@@ -42,7 +48,7 @@ def train(x_train, learning_rate, batch_size, epochs):
 
 
 if __name__ == "__main__":
-    x_train, _, _, _ = load_mnist()
-    autoencoder = train(x_train[:10000], LEARNING_RATE, BATCH_SIZE, EPOCHS)
+    x_train = load_fsdd(SPECTROGRAMS_PATH)
+    autoencoder = train(x_train, LEARNING_RATE, BATCH_SIZE, EPOCHS)
     autoencoder.save("model")
 
