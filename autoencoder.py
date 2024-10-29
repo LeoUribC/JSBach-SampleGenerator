c869fbe1e790348bda8ec366fc88c38d03ca0f0f
LeoUribC <leo.uribe1205@gmail.com>
2024-10-29 11:18:39 -0500

    using mu output

--- autoencoder.py
+++ autoencoder.py
@@ -327,7 +327,7 @@ class VAE:
 
         def sample_point_from_normal_distribution(args):
             mu, log_variance = args
-            epsilon = tf.keras.backend.random_normal(shape=K.shape(self.mu),
+            epsilon = tf.keras.backend.random_normal(shape=tf.shape(self.mu.output),
                                       mean=0.0, stddev=1.0)
             sampled_point = mu + tf.keras.backend.exp(log_variance / 2) * epsilon
             return sampled_point
