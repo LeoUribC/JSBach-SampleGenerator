92ed622575191d8550d669185577362bb567d970
LeoUribC <leo.uribe1205@gmail.com>
2024-10-29 09:05:40 -0500

    using direct Keras model

--- autoencoder.py
+++ autoencoder.py
@@ -14,13 +14,11 @@ import tensorflow as tf
 
 #from tensorflow.keras import Model
 
-from keras.src import Model
-
 from tensorflow.keras.layers import Input, Conv2D, ReLU, BatchNormalization,\
     Flatten, Dense, Reshape, Conv2DTranspose, Activation, Lambda
 from tensorflow.keras.optimizers import Adam
 
-#from keras import Model
+from keras import Model
 #from keras.src.layers import Input, Conv2D, ReLU, BatchNormalization,\
 #    Flatten, Dense, Reshape, Conv2DTranspose, Activation, Lambda
 import keras as K
