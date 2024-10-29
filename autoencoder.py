7533ccdbb2ba72685ad077ad19b9f7810b27a74a
LeoUribC <leo.uribe1205@gmail.com>
2024-10-28 21:46:40 -0500

    using lambda function on exp log variance

--- autoencoder.py
+++ autoencoder.py
@@ -142,7 +142,8 @@ class VAE:
         squared_mu = Lambda(lambda x: tf.square(x))(self.mu)
 
         #log_variance_value = tf.keras.backend.get_value(self.log_variance)
-        exp_log_variance = tf.keras.backend.exp(self.log_variance)
+        #exp_log_variance = tf.keras.backend.exp(self.log_variance)
+        exp_log_variance = Lambda(lambda x: tf.exp(x))(self.log_variance)
 
         kl_loss = -0.5 * tf.keras.backend.sum( 1 + self.log_variance - squared_mu -
                                 exp_log_variance, axis=1 )
