4b233755f002e0832106356985909f75f98ad4a9
LeoUribC <leo.uribe1205@gmail.com>
2024-10-29 09:14:00 -0500

    using keras modules

--- autoencoder.py
+++ autoencoder.py
@@ -14,9 +14,9 @@ import tensorflow as tf
 
 #from tensorflow.keras import Model
 
-from tensorflow.keras.layers import Input, Conv2D, ReLU, BatchNormalization,\
-    Flatten, Dense, Reshape, Conv2DTranspose, Activation, Lambda
-from tensorflow.keras.optimizers import Adam
+# # from tensorflow.keras.layers import Input, Conv2D, ReLU, BatchNormalization,\
+# #     Flatten, Dense, Reshape, Conv2DTranspose, Activation, Lambda
+# # from tensorflow.keras.optimizers import Adam
 
 from keras import Model
 #from keras.src.layers import Input, Conv2D, ReLU, BatchNormalization,\
@@ -25,7 +25,8 @@ import keras as K
 #from keras.src.optimizers import Adam
 import numpy as np
 
-
+from keras.layers import Input, Conv2D, ReLU, BatchNormalization,\
+    Flatten, Dense, Reshape, Conv2DTranspose, Activation, Lambda
 #from keras.src.random import normal
 #from keras.api
 #from keras.src.ops import square, sum, exp
