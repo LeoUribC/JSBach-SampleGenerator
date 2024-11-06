6c98d7250fc28bc8bca421e2c1d265eedd2014db
LeoUribC <leo.uribe1205@gmail.com>
2024-11-06 09:17:14 -0500

    modified duration, must test values

--- preprocess.py
+++ preprocess.py
@@ -240,7 +240,7 @@ if __name__ == "__main__":
 
     FRAME_SIZE = 512
     HOP_LENGTH = 256
-    DURATION = 9.1  # in seconds
+    DURATION = 9.84  # in seconds
     SAMPLE_RATE = 22050
     MONO = True
 
